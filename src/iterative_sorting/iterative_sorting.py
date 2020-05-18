b7bea6a45e5099253fa0b7a3b16ed7ee1213a0bc
Wilma <kima_92@fastmail.com>
2020-05-18 16:48:18 -0400

    Implemented bubble_sort function in iterative_sorting.py. And it successfully passes the test.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,7 +5,8 @@ def selection_sort(arr):
 
     # For each element in the array
     for i in range(0, len(arr)):
-        print(f"i: {i}")
+        #print(f"i: {i}")
+
         # Save the current_index of this element
         cur_index = i
         # Set smallest_index as current_index
@@ -13,7 +14,8 @@ def selection_sort(arr):
 
         # For each element exept current_index ( y )
         for y in range(i + 1, len(arr)):
-            print(f"y {y}")
+            #print(f"y {y}")
+
             # Compare the value in current_index 
             # to the value in smallest_index
 
@@ -34,9 +36,28 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    # Set did_swap bool, to store whether 
+    # we did a swp or not
+    did_swap = True
 
+    # while did_swap is true
+    while did_swap == True:
+        # set did_swap to false
+        did_swap = False
 
+        # Grab the element to the left
+        for left_index in range(0, len(arr)):
+            # Grab the element to the right
+            for right_index in range(left_index + 1, len(arr)):
+
+                # If left is greater than right
+                if arr[left_index] > arr[right_index]:
+                    # Swap
+                    arr[left_index], arr[right_index] = arr[right_index], arr[left_index]
+                    # set did_swap to true
+                    did_swap = True
+                
+    # Return the sorted array
     return arr
 
 
