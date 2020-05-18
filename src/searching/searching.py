209ebce12eaccd6b952d99b71a7dbbb7564a85e8
Wilma <kima_92@fastmail.com>
2020-05-18 17:39:50 -0400

    Implemented the linear_search function in searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,20 @@
+
+
 def linear_search(arr, target):
-    # Your code here
+    # For each index in the array
+    for i in range(0, len(arr)):
+        # If the element at this index
+        # matches the target
+        if arr[i] == target:
+            # return the index in which 
+            # this elements is stored
+            return i
+        
+    
+    # If it never returns True
+    # Return -1
+    return -1
 
-
-    return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
