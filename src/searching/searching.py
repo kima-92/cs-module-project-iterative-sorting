f06ce94ca15de80d9d93baed2ba537448b131d8a
Wilma <kima_92@fastmail.com>
2020-05-18 18:46:05 -0400

    Implemeted the binary_search function in the searching.py. Both tests Pass.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -20,7 +20,44 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    # Set taget_found bool, to store whether you found the taget or not
+    target_found = False
 
+    # Set low to 0
+    low = 0
+    # Set hight to the last index in arr
+    high = len(arr) - 1
 
+    # While target_found is false
+    # and the lenght of arr is not 0
+    while target_found == False and len(arr) != 0:
+
+        # Set the midpoint of arr
+        midpoint = (low + high) // 2
+
+        # If the midpoint is the target
+        if arr[midpoint] == target:
+
+            # Set target_found to true
+            target_found = True
+            # return the index in which the target is at
+            return midpoint
+
+        # If the midpoint is greater than target
+        if arr[midpoint] > target:
+            # We have to focus on the left_side, and
+            # discard the entire right_side
+
+            # Set high to be the midpoint
+            high = midpoint
+
+        # If the midpoint is less than target
+        if arr[midpoint] < target:
+            # We have to focus on the right_side, and
+            # discard the entire left_side
+
+            # Set low as the midpoint
+            low = midpoint
+
+    # If it was not found, return -1
     return -1  # not found
